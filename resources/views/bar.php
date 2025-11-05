356528eca3f132b26c37e4be9764f0c479365844
AdrianoSIO <razanateraa@saintjo.org>
2025-11-05 11:47:53 +0100

    first commit

---
+++ resources/views/bar.php
@@ -0,0 +1,17 @@
+<nav class="navigation" role="navigation">
+  
+      <ul class="navigation-list">
+        <left>
+          <img src="images/logo.png" alt="Logo" class="logo">
+        </left>
+        <li class="navigation-item"><a class="navigation-link" href="accueil">Accueil</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="collège">Collège</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="epreuve">Épreuve</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="classement">Classement</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="contact">Édition</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="equipe">Sécrétaire</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="gestionnaire">Gestionnaire</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="administrateur">Administrateur</a></li>
+        <li class="navigation-item"><a class="navigation-link" href="mentions">Mentions Légales</a></li>
+      </ul>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/accueil', [TestController::class, 'accueil']);
+Route::get('/mentions', [TestController::class, 'mentions']);
+Route::get('/equipe', [TestController::class, 'equipe']);
+Route::get('/', [TestController::class, 'welcome']);
+Route::get('/resultat', [TestController::class, 'role']);
+Route::get('/resultat', [TestController::class, 'genre']);
+Route::get('/resultat', [TestController::class, 'pays']);
+Route::get('/epreuve', [TestController::class, 'epreuve']);
+Route::get('/collège', [TestController::class, 'collège']);
+Route::get('/resultat', [TestController::class, 'resultat']);
